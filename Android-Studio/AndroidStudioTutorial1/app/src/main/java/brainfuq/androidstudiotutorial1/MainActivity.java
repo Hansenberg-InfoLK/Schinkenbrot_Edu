44be6f3004fe1c988dc3358032f6c53707283e8d
David Holland <davidholland5499@outlook.com>
2016-03-01 23:28:58 +0100

    Beispiel App 1 für 2.3.16
    Eine Beispiel App, die zum Gebrauch ab dem 2.3.16 gedacht ist. Für
    Aufgaben zu diesem Part schaue bitte in den Issues nach!

---
+++ Android-Studio/AndroidStudioTutorial1/app/src/main/java/brainfuq/androidstudiotutorial1/MainActivity.java
@@ -0,0 +1,36 @@
+package brainfuq.androidstudiotutorial1;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    TextView textView;
+    EditText editText;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.WelcomeMessage);
+        editText = (EditText) findViewById(R.id.TextField);
+        button = (Button) findViewById(R.id.ApplyButton);
+
+        textView.setText(getString(R.string.welcome_message));
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                textView.setText(getString(R.string.welcome_message_2) + editText.getText());
+            }
+        });
+    }
+}
